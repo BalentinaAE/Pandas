2285cc54d636a0379ccdb8a589006317afc7264f
BalentinaAE <bagudeloe@gmail.com>
2024-11-03 23:00:38 +0100

    Primer commit: subir archivos

---
+++ clase_nuevo_profe.py
@@ -0,0 +1,5 @@
+a= [10, "Hola", -294] # lita--- vector matematico,,, que puede pasar a una matriz--
+b={"key":"value", 9:10,"isaac":{"id" : 434}}  #Es un diccionario ... es una lista especoal, cada elemento tiene dos cosas
+# como la capacidad de ir rapido al dato que tenemos esos : lo dicen.... enisacc el valor puede haber otro diccionario
+c= (43.909, 2.0001) #tupla---"un punto dentro de un espacio"
+import pandas as pd
